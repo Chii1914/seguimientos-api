a1510a49c2f73b128317ca96361b229e1004c178
Chii1914 <maximiliano.aguirre@alumnos.uv.cl>
2024-12-19 14:23:22 +0000

    conexion base de datos

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as cors from 'cors'; // Importa el paquete 'cors'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.use(cors({
+    origin: 'http://localhost:3001',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization', // Include Authorization
+    credentials: true,
+  })); 
+  app.setGlobalPrefix("api/");
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,              //Usuario envía cosas que no corresponda
+      forbidNonWhitelisted: true,   //No permite enviar cosas que no correspondan
+      transform: true,              //Transforma los datos que recibe a los tipos que se le indican
+    })
+  );
+  await app.listen(process.env.OPERATION_PORT || 3000);
 }
 bootstrap();
