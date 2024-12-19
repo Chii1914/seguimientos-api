731deb0f6f91153a3225747be33a1802b0980810
EddieKiller <nataniel.palacios@alumnos.uv.cl>
2024-12-19 16:14:07 -0300

    creacionDtoFollowUp

--- src/follow-up/dto/create-follow-up.dto.ts
+++ src/follow-up/dto/create-follow-up.dto.ts
@@ -1,1 +1,44 @@
-export class CreateFollowUpDto {}
+import { IsBoolean, IsDate, IsEmail, IsIn, IsInt, IsOptional, IsString } from "class-validator";
+
+export class CreateFollowUpDto {
+
+    @IsDate()
+    timestamp: Date;
+
+    @IsEmail()
+    mail: string;
+
+    @IsString()
+    notes: string;
+
+    @IsOptional()
+    @IsBoolean()
+    ajusteAcademico: boolean;
+
+    @IsOptional()
+    @IsString()
+    justAjusteAcademico: string;
+
+    @IsOptional()
+    @IsBoolean()
+    documentosRespaldo: boolean;
+
+    @IsOptional()
+    @IsString()
+    justDocumentosRespaldo: string;
+
+    @IsOptional()
+    @IsBoolean()
+    noAceptaIndicaciones: boolean;
+
+    @IsOptional()
+    @IsString()
+    justNoAcpetaIndicaciones: string;
+
+    @IsOptional()
+    @IsString()
+    otro: string;
+
+    
+
+}
--- src/follow-up/follow-up.service.ts
+++ src/follow-up/follow-up.service.ts
@@ -1,26 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { CreateFollowUpDto } from './dto/create-follow-up.dto';
 import { UpdateFollowUpDto } from './dto/update-follow-up.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { FollowUp } from './entities/follow-up.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class FollowUpService {
-  create(createFollowUpDto: CreateFollowUpDto) {
+
+  constructor (
+
+    @InjectRepository(FollowUp) private followUpRepository: Repository<FollowUp>
+
+  ){}
+
+  async create(createFollowUpDto: CreateFollowUpDto) {
     return 'This action adds a new followUp';
   }
 
-  findAll() {
+  async findAll() {
     return `This action returns all followUp`;
   }
 
-  findOne(id: number) {
+  async findOne(id: number) {
     return `This action returns a #${id} followUp`;
   }
 
-  update(id: number, updateFollowUpDto: UpdateFollowUpDto) {
+  async update(id: number, updateFollowUpDto: UpdateFollowUpDto) {
     return `This action updates a #${id} followUp`;
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return `This action removes a #${id} followUp`;
   }
 }
