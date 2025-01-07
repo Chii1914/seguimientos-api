cad20f470846051081079824e9ea5677abcbe743
Chii1914 <maximiliano.aguirre@alumnos.uv.cl>
2025-01-07 15:52:30 -0300

    feat(user): add custom request interface and integrate JWT module for authentication

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,15 +4,17 @@ import { UserController } from './user.controller';
 import { TypeORMError } from 'typeorm';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Global()
 @Module({
 
-  imports: [
-    TypeOrmModule.forFeature([User])
-  ],
+  imports: [TypeOrmModule.forFeature([User]), JwtModule.register({
+    secret: process.env.JWT_SECRET_KEY,
+    signOptions: { expiresIn: '300m' },
+  }),],
   controllers: [UserController],
   providers: [UserService],
   exports: [TypeOrmModule.forFeature([User])]
 })
-export class UserModule {}
+export class UserModule { }
